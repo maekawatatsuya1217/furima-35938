a365ea2c191b4b885835d21b542c6db18029812d
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-06-16 11:28:40 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name, presence: true, format: {with: /\A[一-龥ぁ-ん]/ }
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
   
 end
