4e4ec514e0f7a8483c3adbd305d046b448b56d45
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-06-18 18:12:15 +0900

    レビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :unless, only: [:edit, :update, :dest, :destroy]
+  before_action :unless, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
