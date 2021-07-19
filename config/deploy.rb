a0f5cbc99c32341ef13cd7a3aadfc30f43fd0ee0
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-19 14:21:26 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-35938'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'maekawatatsuya1217/furima-35938.git'
+set :repo_url,  'git@github.com:maekawatatsuya1217/furima-35938.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
